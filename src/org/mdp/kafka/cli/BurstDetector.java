45621741d299a09c625ef4fc4529bd4655eb21e9
Vicen <vicentemoriarti@gmail.com>
2024-07-04 03:48:16 -0400

    añadido resultados

--- src/org/mdp/kafka/cli/BurstDetector.java
+++ src/org/mdp/kafka/cli/BurstDetector.java
@@ -16,9 +16,15 @@ import java.util.UUID;
 
 public class BurstDetector {
     public static final String[] CRIME_SUBSTRINGS = new String[] { "sex abuse", "theft" };
+    // Tamaño del buffer FIFO para almacenar registros de crímenes detectados
     public static final int FIFO_SIZE = 3;
-    public static final int EVENT_START_TIME_INTERVAL = 1 * 60 * 60 * 1000; // convertir a milisegundos
-    public static final int EVENT_END_TIME_INTERVAL = 6 * 60 * 60 * 1000; // convertir a milisegundos
+
+    // Intervalo de inicio de evento en horas
+    public static final int EVENT_START_TIME_INTERVAL = 1 * 60 * 60 * 1000; // 1 hora
+
+    // Intervalo de fin de evento en horas
+    public static final int EVENT_END_TIME_INTERVAL = 6 * 60 * 60 * 1000; // 6 horas
+
 
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
@@ -26,6 +32,7 @@ public class BurstDetector {
             System.err.println("Usage [inputTopic] [blocks]");
             return;
         }
+        // Palabras clave de zonas donde se filtran los crímenes
         String[] ZONE_SUBSTRINGS = args[1].split(",");
         for (int i = 0; i < ZONE_SUBSTRINGS.length; i++) {
                  ZONE_SUBSTRINGS[i] = ZONE_SUBSTRINGS[i].toLowerCase();
@@ -33,10 +40,9 @@ public class BurstDetector {
 
         Properties props = KafkaConstants.PROPS;
 
-        // randomise consumer ID so messages cannot be read by another consumer
-        //   (or at least it's more likely that a meteor wipes out life on Earth)
+
         props.put(ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID().toString());
-        // Initialize FIFO
+
         LinkedList<ConsumerRecord<String, String>> fifo = new LinkedList<ConsumerRecord<String, String>>();
         KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
         // initialize state variables
@@ -47,12 +53,13 @@ public class BurstDetector {
 
         try{
             while (true) {
-                // every ten milliseconds get all records in a batch
+                // Obtener todos los registros en lotes cada 10 minutos
                 ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000*60));
 
-                // for all records in the batch
+                // Procesar todos los registros en el lote
                 for (ConsumerRecord<String, String> record : records) {
                     String lowercase = record.value().toLowerCase();
+                     Verificar si el valor del registro contiene alguna palabra clave de crimen
 
                     boolean breaking = false;
                     for(String cr: CRIME_SUBSTRINGS){
@@ -61,11 +68,10 @@ public class BurstDetector {
 								break;
 							}
                         	for(String zs: ZONE_SUBSTRINGS) {
-                        		// if so print it out to the console
+
                         		if(lowercase.contains(zs)) {
                         			breaking = true;
                         			fifo.add(record);
-                        			// prevents multiple prints of the same crime with multiple keywords
                         			if(fifo.size() >= FIFO_SIZE){
                         				ConsumerRecord<String, String> oldest = fifo.removeFirst();
                         				long gap = record.timestamp() - oldest.timestamp();
